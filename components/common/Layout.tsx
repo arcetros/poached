b94095ab8900a1730ccf666f66ceafe71de5cff8
arcetros <75669794+arcetros@users.noreply.github.com>
2022-12-07 19:13:21 +0800

    chore(Navbar): implement responsiveness

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -8,13 +8,33 @@ type Props = {
 }
 
 const Layout: React.FunctionComponent<Props> = ({ children }) => {
+  const [isNavbarOpen, setIsNavbarOpen] = React.useState<boolean>(false)
+  const [clientWidth, setClientWidth] = React.useState<number | undefined>(undefined)
+
+  const ref = React.useRef<HTMLDivElement>(null)
+
+  const toggleNavBar = () => setIsNavbarOpen((state) => !state)
+
+  React.useEffect(() => {
+    function handleResize() {
+      setClientWidth(ref.current?.offsetWidth)
+    }
+    window.addEventListener("resize", handleResize)
+
+    if (clientWidth && clientWidth > 768) {
+      setIsNavbarOpen(false)
+    }
+
+    return () => window.removeEventListener("resize", handleResize)
+  }, [ref.current?.offsetWidth])
+
   return (
-    <main className="flex h-auto flex-col overflow-hidden md:h-screen md:flex-row">
+    <main ref={ref} className="flex h-auto flex-col overflow-hidden md:h-screen md:flex-row">
       <SEO>
         <meta key="viewport" name="viewport" content="width=device-width, initial-scale=1" />
       </SEO>
-      <Navbar />
-      <div className="w-full">{children}</div>
+      <Navbar isOpen={isNavbarOpen} toggleNavBar={toggleNavBar} />
+      <div className="w-full overflow-auto pt-[90px] md:pl-[15rem] md:pt-0">{children}</div>
     </main>
   )
 }
